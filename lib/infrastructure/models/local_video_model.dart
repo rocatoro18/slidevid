a33e992b19eacdab8e8dd271e68a399fc46defd6
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-11-27 22:25:28 -0700

    Local Video Model

---
+++ lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,24 @@
+import 'package:slidevid/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  LocalVideoModel(
+      {required this.name,
+      required this.videoUrl,
+      this.likes = 0,
+      this.views = 0});
+
+  factory LocalVideoModel.fromJsonMap(Map<String, dynamic> json) =>
+      LocalVideoModel(
+          name: json['name'] ?? 'No caption',
+          videoUrl: json['videoUrl'],
+          likes: json['likes'] ?? 0,
+          views: json['views'] ?? 0);
+
+  VideoPost toVideoPostEntity() =>
+      VideoPost(caption: name, videoUrl: videoUrl, likes: likes, views: views);
+}

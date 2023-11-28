3bce06a958fa5c70f80be47c4891ad184196a0c2
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-11-28 15:48:58 -0700

    CircularProgressIndicator & PageView

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:slidevid/presentation/providers/discover_provider.dart';
+import 'package:slidevid/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Discover Screen'),
-      ),
+    // REFERENCIA AL DISCOVER PROVIDER
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    // ESTO ES LO MISMO QUE EL DISCOVER PROVIDER
+    //final otroProvider = Provider.of<DiscoverProvider>(context, listen: false);
+
+    return Scaffold(
+      body: discoverProvider.initialLoading
+          ? const Center(
+              child: CircularProgressIndicator(strokeWidth: 2),
+            )
+          : VideoScrollableView(videos: discoverProvider.videos),
     );
   }
 }
---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:slidevid/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  const VideoScrollableView({super.key, required this.videos});
+
+  final List<VideoPost> videos;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+        Container(color: Colors.amber)
+      ],
+    );
+  }
+}

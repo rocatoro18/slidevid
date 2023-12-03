6def58211881f9fc1faf4488fb437a2bf1cc087d
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-02 22:59:09 -0700

    Controlador del video - Future Builder

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   const FullScreenPlayer(
       {super.key, required this.videoUrl, required this.caption});
 
@@ -8,7 +9,39 @@ class FullScreenPlayer extends StatelessWidget {
   final String caption;
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          // SNAPSHOT ES EL ESTADO DE ESTE FUTURE
+          return const Center(
+            child: CircularProgressIndicator(strokeWidth: 2),
+          );
+        });
   }
 }
